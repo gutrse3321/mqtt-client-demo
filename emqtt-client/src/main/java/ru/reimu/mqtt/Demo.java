cd61c9882e9703c779e1ed1b9c070543ca36fd0d
gutrse3321 <gutrse3321@live.com>
2020-02-07 01:29:30 -0800

    java client & javascript client

---
+++ emqtt-client/src/main/java/ru/reimu/mqtt/Demo.java
@@ -0,0 +1,69 @@
+package ru.reimu.mqtt;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.text.MessageFormat;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        String broker = "tcp://127.0.0.1:1883";
+        String clientId = "JavaSample";
+
+        //use the memory persistence
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient client = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+
+            System.out.println("connecting to broker");
+
+            client.connect(connectOptions);
+
+            System.out.println("connected");
+
+            /**
+             * subscribe
+             */
+            String topic = "demo/topics";
+            System.out.println("subscribe to topic: " + topic);
+
+            client.subscribe(topic);
+
+            client.setCallback(new MqttCallback() {
+                public void connectionLost(Throwable throwable) {
+
+                }
+
+                public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                    String msg = MessageFormat.format("msg: {0}, topic: {1}", mqttMessage.toString(), topic);
+                    System.out.println(msg);
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+                }
+            });
+
+            /**
+             * publish
+             */
+            String pubMsg = "message from MqttPublish - java";
+            int qos = 2;
+            System.out.println("[Publish] publish message: " + pubMsg);
+            MqttMessage pubMsgByte = new MqttMessage(pubMsg.getBytes());
+            pubMsgByte.setQos(qos);
+            client.publish(topic, pubMsgByte);
+            System.out.println("[Publish] Message published");
+        } catch (MqttException e) {
+            System.out.println("reason:" + e.getReasonCode());
+            System.out.println("msg:" + e.getMessage());
+            System.out.println("loc:" + e.getLocalizedMessage());
+            System.out.println("cause:" + e.getCause());
+            System.out.println("excep:" + e);
+        }
+    }
+}
---
+++ mqtt-js/main.js
@@ -0,0 +1,52 @@
+let mqtt = require('mqtt')
+
+const OPTS = {
+  connectionTimeout: 4000,
+  clientId: 'JsSample',
+  keepalive: 60,
+  clean: true
+}
+
+const BASE_URL = '127.0.0.1'
+const WENSOCKET_URL = `ws://${BASE_URL}:8083/mqtt`
+//tcp node
+const TCP_URL = `mqtt://${BASE_URL}:1883`
+const TCP_TLS_URL = `mqtt://${BASE_URL}:8883`
+
+const client = mqtt.connect(TCP_URL, OPTS)
+
+/**
+ * subscribe
+ */
+const TOPIC = "demo/topics"
+client.subscribe(
+  [TOPIC],
+  {
+    qos: 1
+  },
+  err => {
+    console.log('subscribe success')
+  }
+)
+client.on('message', (topic, message) => {
+  console.log(`[subscribe]topic: ${topic}, msg: ${message.toString()}`)
+})
+
+/**
+ * publish
+ */
+client.publish('demo/topics', 'message from MqttPublish - JavaScript', err => {
+  console.log(err || '[publish] success')
+})
+
+client.on('connect', () => {
+  console.log('connected')
+})
+
+client.on('reconnect', error => {
+  connect('reconnecting: ', error)
+})
+
+client.on('error', error => {
+  console.log('error connect: ', error)
+})
